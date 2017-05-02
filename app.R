4390b07cc46fd1836bb6cd895f406b15881b9211
marchan001 <andrea.marchini@pirelli.com>
2017-05-03 00:47:11 +0200

    Code grooming

--- app.R
+++ app.R
@@ -1,31 +1,28 @@
 # global.R -----------------------------------------------------------------
 
-#rm(list = ls())
-
+library(dplyr)
+library(ggmap)
 library(shiny)
 library(shinydashboard)
-library(ggmap)
-library(dplyr)
 
 source("load_data.R")
-#source("ggplot.R")
-
 
 tbl_eq <- load_data()
 source("ggmap.R")
 if (!exists("map_eq")) map_eq <- init_map(tbl_eq)
+
 # ui.R ----------------------------------------------------------------------
 
-ui <- ui <- dashboardPage(
+ui <- dashboardPage(
 	dashboardHeader(title = "Filters"),
 	dashboardSidebar(
 		sliderInput("range", "Years:", 
 								min = min(tbl_eq$year), max = max(tbl_eq$year), 
 								value = c(1985, 1985) , step = 1,
-								animate = animationOptions(interval=1000, loop=T)),
+								animate = animationOptions(interval = 1000, loop = TRUE)),
 		sliderInput("range_mag", "Magnitudo:", 
 								min = min(tbl_eq$mag), max = max(tbl_eq$mag), 
-								value = c(2, 6.5)    , step = 0.5 )
+								value = c(2, 6.5), step = 0.5 )
 	),
 	dashboardBody(
 		# Boxes need to be put in a row (or column)
@@ -39,19 +36,16 @@ ui <- ui <- dashboardPage(
 
 # server.R ----------------------------------------------------------------
 
-library(shiny)
-
 server <- function(input, output) {
 
 		output$plot1 <- renderPlot({
-		plot_map(tbl_eq, map_eq,
-						 timeRange = input$range,
-						 magnitudeRange = input$range_mag)
+			plot_map(tbl_eq, map_eq,
+							 timeRange = input$range,
+							 magnitudeRange = input$range_mag)
 		})
 		
 }
 
-
 # launch-app --------------------------------------------------------------
 
-shiny::shinyApp(ui = ui, server = server)
+shinyApp(ui = ui, server = server)
